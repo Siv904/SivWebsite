f98a80d307b5fb9d9ba8a0c5454ba3ec2e385df2
siv904 <89902229+Siv904@users.noreply.github.com>
2021-10-04 13:09:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template
+
+# Create a flask app
+app = Flask(
+  __name__,
+  template_folder='templates',
+  static_folder='static'
+)
+
+# Index page (now using the index.html file)
+@app.route('/') #Mainpage
+def index():
+  return render_template('index.html')
+
+@app.route('/shop/') #Shop page
+def shop():
+  return render_template('shop.html')
+
+@app.route('/shop2/') #Shop page
+def shop2():
+  return render_template('shop.html')
+
+@app.route('/about-me/') #About me page
+def aboutme():
+  return render_template('aboutme.html')
+
+@app.route('/cart/') #About me page
+def cart():
+  return render_template('cart.html')
+
+if __name__ == '__main__':
+  # Run the Flask app
+  app.run(
+	host='0.0.0.0',
+	debug=True,
+	port=8080
+  )
+
+  # To open website localhost:8080
