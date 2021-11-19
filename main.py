99f4d81f21a48e222f69949a48dc1b988f2c0745
Siv <89902229+Siv904@users.noreply.github.com>
2021-11-19 07:29:07 +0000

    Changed buttons,

--- main.py
+++ main.py
@@ -1,44 +1,37 @@
 from flask import Flask, render_template
 
 # Create a flask app
-app = Flask(
-  __name__,
-  template_folder='templates',
-  static_folder='static'
-)
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
 
 # Index page (now using the index.html file)
-@app.route('/') #Mainpage
+@app.route('/')  #Mainpage
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/shop/') #Shop page
+
+@app.route('/shop/')  #Shop page
 def shop():
-  return render_template('shop.html')
+    return render_template('shop.html')
 
-@app.route('/shop2/') #Shop page
-def shop2():
-  return render_template('shop.html')
 
-@app.route('/about-me/') #About me page
+@app.route('/about-me/')  #About me page
 def aboutme():
-  return render_template('aboutme.html')
+    return render_template('aboutme.html')
 
-@app.route('/cart/') #Cart page
+
+@app.route('/cart/')  #Cart page
 def cart():
-  return render_template('cart.html')
+    return render_template('cart.html')
 
-@app.route('/shop/item/') #Item page
+
+@app.route('/shop/item/')  #Item page
 def item():
-  return render_template('item.html')
+    return render_template('item.html')
 
 
 if __name__ == '__main__':
-  # Run the Flask app
-  app.run(
-	host='0.0.0.0',
-	debug=True,
-	port=8080
-  )
+    # Run the Flask app
+    app.run(host='0.0.0.0', debug=True, port=8080)
 
-  # To open website localhost:8080
+    # To open website localhost:8080
